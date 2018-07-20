b1654a400c56d774a42edd380e7f104bb2960904
mzfr <mehtab.zafar98@gmail.com>
2018-07-20 22:37:56 +0530

    Add 404 page
    flask will handle error for any non existing page

--- app/routes.py
+++ app/routes.py
@@ -14,4 +14,12 @@ def index():
 def search():
     query = request.form['search']
     data = wca(query)
-    return render_template('table.html', data=data)
+    if data:
+        return render_template('table.html', data=data)
+    else:
+        return render_template('No_comp.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
