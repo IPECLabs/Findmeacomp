18c3de81f6faf5a1ec35a36395bd070dbc850d52
ugtan <umang.taneja0173@gmail.com>
2018-07-25 22:54:51 +0530

    Read country codes from json and use geopy
    Updated fide_scraper to read for country codes
    from json files instead of using pycountry.
    Also, it uses geopy library for getting the
    country.

--- app/fide_scraper.py
+++ app/fide_scraper.py
@@ -1,19 +1,31 @@
+import json
 import requests
-import pycountry
 
 from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
+
+
+def getCountry(city):
+    """To find out the country based on the city entered by the user"""
+    geolocator = Nominatim(user_agent="my-application")
+    location = geolocator.geocode(city, language='en')
+    country = str(location.address).replace(" ", "").split(",")[-1]
+    return country
 
 
 def getCountryCode(country):
-    """To get the country code in alpha3 format by using the country's name"""
-    for c in pycountry.countries:
-        if country == c.name.lower():
-            return c.alpha_3
+    """To get the country code of the country entered by the user. It reads the country codes from a json file CountryCodes.json"""
+    with open('CountryCodes.json') as f:
+        data = json.load(f)
+    for d in data:
+        if d['name'].lower() == country:
+            return d['code']
 
 
-def getList(soup, city = None):
-    """To get the list of the available tournaments"""
-    table = soup.find('div',{'id':'main-col'}).find_all('table')[1].find_all('tr')[2].find('td').find_all('table')[1]
+def getList(soup, city=None):
+    """To get the list of available tournaments"""
+    table = soup.find('div', {'id': 'main-col'}).find_all('table')[
+        1].find_all('tr')[2].find('td').find_all('table')[1]
     trs = table.find_all('tr', bgcolor=True)
     myList = [tr.text for tr in trs]
     myList = [x.split('\xa0') for x in myList]
@@ -23,11 +35,10 @@ def getList(soup, city = None):
 
     for i in new_list:
         i[1] = i[1][:-1]
-
-    for i in new_list:
-        if city is not None:
-            if i[1].lower() == city:
-                nlist.append(list(i))
+        if city:
+            for c in i[1].lower().split(', '):
+                if c == city:
+                    nlist.append(list(i))
 
         else:
             return new_list
@@ -36,14 +47,14 @@ def getList(soup, city = None):
 
 
 def main():
-    city = input("Enter City Name!")
-    country = input("Enter Country Name!")
-    countryCode = getCountryCode(country)
+    city =input('Enter your city name! ')
+    country = getCountry(city)
+    countryCode = getCountryCode(country.lower())
     url = 'https://ratings.fide.com/tournament_list.phtml?moder=ev_code&country={}'.format(countryCode)
     res = requests.get(url)
     soup = BeautifulSoup(res.content, "lxml")
-    myList = getList(soup, city)
-    print(myList)
+    List = getList(soup, city)
+    print(List)
 
 
 if __name__ == '__main__':
